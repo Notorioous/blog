17260482b91d0d876b3c24dfdf82db56b5840bd0
Notorioous <47818818+Notorioous@users.noreply.github.com>
2019-03-11 00:29:00 +0400

    Update PostRepository.java

--- src/main/java/com/example/blog/repository/PostRepository.java
+++ src/main/java/com/example/blog/repository/PostRepository.java
@@ -7,5 +7,5 @@ import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
 
-    List<Post> findAllByCategoriesId(int category_id);
+    List<Post> findAllByCategoriesId(int categoryId);
 }
